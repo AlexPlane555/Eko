1965cfc3a273d681f4f094f5b4563d79785d35e6
AlexPlane555 <alexey.gass555@mail.ru>
2024-11-07 14:20:00 +0300

    Add files via upload

---
+++ js/indexjs.js
@@ -0,0 +1,21 @@
+// Скрываем все страницы, кроме главной
+const pages = document.querySelectorAll('.page');
+pages.forEach(page => page.style.display = 'none');
+document.getElementById('home').style.display = 'block';
+
+// Добавляем обработчик клика для ссылок в меню
+const sidebarLinks = document.querySelectorAll('.sidebar a');
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        // Получаем ID страницы
+        const pageId = link.getAttribute('href');
+
+        // Скрываем все страницы
+        pages.forEach(page => page.style.display = 'none');
+
+        // Отображаем нужную страницу
+        document.querySelector(pageId).style.display = 'block';
+    });
+});
